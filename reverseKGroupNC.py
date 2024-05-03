bb5ebdeb67c60d2bc7f44afe4da43612b7659773
Peter Koverda <peter@pulsegrow.com>
2024-05-03 12:17:56 -0700

    nc solution with notes

--- reverseKGroupNC.py
+++ reverseKGroupNC.py
@@ -5,6 +5,37 @@ class ListNode:
 
 class ReverseKGroup:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            # reverse group
+            prev, curr = kth.next, groupPrev.next
+
+            while curr != groupNext:
+                ptr = curr.next     # get next
+                curr.next = prev    # point back
+                prev = curr         # set next prev
+                curr = ptr          # iterate
+
+            tmp = groupPrev.next # save dummy.next ptr (#1)
+            groupPrev.next = kth # set dummy.next to kth (#2, etc)
+            groupPrev = tmp # set grpprev to next group prv, no longer messing with dummy's ptr in future iterations!
+        return dummy.next
+
+
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+
 
 def printNodes(l: ListNode):
     while l:
@@ -13,9 +44,8 @@ def printNodes(l: ListNode):
     print("")
 
 
-
-
-
 rol = ReverseKGroup()
-ll = ListNode(1, ListNode(2, ListNode(3)))
+ll = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+printNodes(ll)
 res = rol.reverseKGroup(ll, 2)
+printNodes(res)
