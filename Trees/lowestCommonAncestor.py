dd1b087b867f124fd9a2deaaf5ab497a42a840ef
Peter Koverda <peter@pulsegrow.com>
2024-05-03 15:29:58 -0700

    235. lowest common ancestor

---
+++ Trees/lowestCommonAncestor.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+
+        if q.val < p.val:
+            q, p = p, q
+
+        if root.val >= p.val and root.val <= q.val:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left: return left
+        if right: return right
+        return None
