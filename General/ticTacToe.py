84a5ad0856b96c18b7eb05c27ac24511974f0c42
Peter Koverda <peter@pulsegrow.com>
2024-05-07 08:14:38 -0700

    starting on win condition

--- General/ticTacToe.py
+++ General/ticTacToe.py
@@ -1,3 +1,6 @@
+import os
+
+
 class TicTacToe:
     board = []
     gameOver = False
@@ -16,15 +19,35 @@ class TicTacToe:
             print("")
         print("")
 
+    # starts from top left
+    # column coords are letters
+    # row coords are numbers
+    # a1 = top left
     def markSquare(self, letter, number, marker):
-        i = int(number) - 1
-        l = int(ord(letter[0].lower()) - ord('a'))
-        self.board[i][l] = marker
+        row = int(number) - 1
+        column = int(ord(letter[0].lower()) - ord('a'))
+        self.board[row][column] = marker
         self.drawBoard()
 
+    def checkWin(self):
+        # check horiz
+        for i in range(len(self.board[0])):
+            char = self.board[i]
+            for j in range(len(self.board)):
+                print(self.board[i][j])
+                if self.board[i][j] != char:
+                    return False
+
+
+        # check vert
+
+        # check diag
 
 
 ttt = TicTacToe()
 ttt.drawBoard()
 ttt.markSquare('a', 1, 'x')
-ttt.markSquare('b', 3, 'o')
+ttt.markSquare('b', 1, 'x')
+ttt.markSquare('c', 1, 'x')
+res = ttt.checkWin()
+print(res)
