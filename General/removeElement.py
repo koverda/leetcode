011c2d642e003bb2f0aa6cfac9e8d87f0a7797d8
Peter Koverda <peter@pulsegrow.com>
2024-06-05 10:16:44 -0700

    better sln

--- General/removeElement.py
+++ General/removeElement.py
@@ -4,21 +4,10 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
-        j = 0
-
-        for i in range(len(nums)):
-            if nums[i] == val:
-                j = i
-                while j < len(nums)-1 and nums[j] == val:
-                    j += 1
-                if nums[j] != val:
-                    nums[i] = nums[j]
-                    nums[j] = val
-                    i += 1
-                if j == len(nums) - 1:
-                    return i
-            i += 1
-            j += 1
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
         return i
 
 s = Solution()
